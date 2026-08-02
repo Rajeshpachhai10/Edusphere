4ee9bea427c9c236a3ced5e16b10000656f1f7fa
Rajesh Pachhai <pachhairajesh30@gmail.com>
2026-08-02 23:27:21 +0545

    Project ready to deploy

--- edusphere/settings.py
+++ edusphere/settings.py
@@ -94,12 +94,27 @@ WSGI_APPLICATION = "edusphere.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
+
+# Postgress connect in django
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER': 'postgres',
+        'PASSWORD': config('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
